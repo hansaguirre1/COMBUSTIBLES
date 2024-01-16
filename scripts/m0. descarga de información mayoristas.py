07c1f32779c15cc0595a4a50b7b85673cfc10efc
Hans Aguirre <81196895+hansaguirre1@users.noreply.github.com>
2024-01-16 15:27:45 -0500

    Hans 2024.01.16

---
+++ scripts/m0. descarga de información mayoristas.py
@@ -0,0 +1,68 @@
+#importar libreria
+
+from selenium import webdriver
+import re
+import time 
+import os
+import random
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from datetime import date, datetime, timedelta
+import json
+import pytz 
+from selenium.webdriver import ActionChains
+import statistics
+import requests
+import os, zipfile
+
+
+# Configuración e inicio de navegación
+
+dir=os.getcwd()
+
+new_dir_path = f'{dir}\\data\\raw\\precios mayoristas'
+chrome_options = webdriver.ChromeOptions()
+service = Service(executable_path=r'E:/GitHub/COMBUSTIBLES/chromedriver.exe')
+home_dir = './scraper_combustibles'
+prefs = {'download.default_directory' : new_dir_path,  
+         "download.prompt_for_download": False,
+         "download.directory_upgrade": True,
+         "plugins.always_open_pdf_externally": True}  # Esto evita que Chrome abra el PDF en el visor incorporado
+chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36")
+chrome_options.add_experimental_option('prefs', prefs)
+
+# Abrir navegador 
+
+driver = webdriver.Chrome(service=service, options=chrome_options)
+url_1 ='https://www.osinergmin.gob.pe/empresas/hidrocarburos/scop/documentos-scop'
+driver.get(url_1)
+
+# Encontrar elementos de interés
+driver.find_element(By.XPATH, '//*[@id="browser"]/li[13]/div').click()
+
+# Descarga de información
+
+for element in container_list:
+    for i in range(0,1):
+        element.click()
+        time.sleep(5)
+
+# Unzip files
+
+extension = ".zip"
+os.chdir(new_dir_path)
+
+for file in os.listdir(new_dir_path):   # get the list of files
+    if zipfile.is_zipfile(file): # if it is a zipfile, extract it
+        with zipfile.ZipFile(file) as item: # treat the file as a zip
+           item.extractall()
+
+
---
+++ scripts/m1. join y limpieza de información.py
@@ -0,0 +1,202 @@
+# importar libreria
+from selenium import webdriver
+import re
+import time 
+import os
+import random
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+import json
+import pytz 
+import statistics
+import requests
+import os, zipfile
+
+# Declarar ruta
+dir=os.getcwd()
+
+new_dir_path = f'{dir}\\data\\raw\\precios mayoristas'
+
+# Eliminar todo lo que no es excel
+
+for file in os.listdir(new_dir_path):
+    if not file.endswith('.xlsx'):
+        file_path = os.path.join(new_dir_path, file)
+        os.remove(file_path)
+        print(f"Deleted: {file_path}")
+
+# Guardar todo en lista
+
+Precios_Mayoristas_list=[]
+for file in os.listdir(new_dir_path):
+    if file.endswith('.xlsx'):
+        df=pd.read_excel(f'{new_dir_path}\\{file}')
+        try:
+            row_index = df.index[df['Unnamed: 1'] == 'ACTIVIDAD'].tolist()[0]
+            df.columns = df.iloc[row_index]
+        except:
+            pass
+        df["Tipo"]=file
+        Precios_Mayoristas_list.append(df)
+
+# Filtar por actividades de interés
+b=[]
+for a in Precios_Mayoristas_list:
+    a=a.drop(columns="NaN", errors='ignore')
+    b.append(a.loc[ (a['ACTIVIDAD'].str.contains('DISTRIBUIDOR')) |
+                    (a['ACTIVIDAD'].str.contains('PRODUCTOR')) |
+                    (a['ACTIVIDAD'].str.contains('IMPORTADOR')) |
+                    (a['ACTIVIDAD'].str.contains('PLANTA'))|
+                    (a['ACTIVIDAD'].str.contains('MAYORISTAS'))|
+                    (a['ACTIVIDAD'].str.contains('ACTIVIDAD'))|
+                    (a['ACTIVIDAD'].str.contains('PROCESAMIENTO'))|
+                    (a['ACTIVIDAD'].str.contains('COMERCIALIZADOR DE GLP'))
+                    ])
+
+# join todas las bases 
+
+Total=pd.concat(b)
+
+# Renombrar productos y codificar productos de interes
+
+Precios_Mayoristas=Total
+
+# Homogenizar producto
+Precios_Mayoristas['PRODUCTO']=Precios_Mayoristas['PRODUCTO'].str.replace("GASOHOL 95 PLUS","GASOHOL PREMIUM")
+Precios_Mayoristas['PRODUCTO']=Precios_Mayoristas['PRODUCTO'].str.replace("GASOHOL 90 PLUS","GASOHOL REGULAR")
+
+Precios_Mayoristas['PRODUCTO']=Precios_Mayoristas['PRODUCTO'].str.replace("GOH95","GASOHOL PREMIUM")
+Precios_Mayoristas['PRODUCTO']=Precios_Mayoristas['PRODUCTO'].str.replace("GOH90","GASOHOL REGULAR")
+
+Precios_Mayoristas['PRODUCTO']=Precios_Mayoristas['PRODUCTO'].str.replace("GASOLINA 90","GASOLINA REGULAR")
+Precios_Mayoristas['PRODUCTO']=Precios_Mayoristas['PRODUCTO'].str.replace("GASOLINA 95","GASOLINA PREMIUM")
+Precios_Mayoristas['PRODUCTO']=Precios_Mayoristas['PRODUCTO'].str.replace("G90","GASOLINA REGULAR")
+Precios_Mayoristas['PRODUCTO']=Precios_Mayoristas['PRODUCTO'].str.replace("G95","GASOLINA PREMIUM")
+
+Precios_Mayoristas['PRODUCTO']=Precios_Mayoristas['PRODUCTO'].str.replace("'Cilindros de 10 Kg de GLP","GLP - E")
+
+# Renombrar variables de ubicación
+
+Precios_Mayoristas=Precios_Mayoristas.rename(columns={'DEPARTAMENTO':'DEPARTAMENTO_VENDEDOR',
+                                   'PROVINCIA':'PROVINCIA_VENDEDOR',
+                                   'DISTRITO':'DISTRITO_VENDEDOR'
+                                   })
+
+# Eliminar productos que no utilizamos
+
+Precios_Mayoristas = Precios_Mayoristas[~(Precios_Mayoristas['PRODUCTO'].str.contains('Cilindros de 5 Kg de GL')) &
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('ASFALTO'))&
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('PETRÓLEO'))&
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('CEMENTO'))&
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('OIL'))&
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('HEXANO'))&
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('Cilindros de 45 Kg de GLP'))&
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('Cilindros de 15 Kg de GLP'))&
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('Cilindros de 3 Kg de GLP'))&
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('GLP - E'))&
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('MARINO'))&
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('TURBO'))&
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('IFO - 380 EXPORT'))&
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('PENTANO'))&
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('BREA'))&
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('GASOLINA 84'))&
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('GASOHOL 84 PLUS'))&
+                                        ~(Precios_Mayoristas['PRODUCTO']=='DIESEL B5')&
+                                        ~(Precios_Mayoristas['PRODUCTO']=='DIESEL B5 GE')&
+                                        ~(Precios_Mayoristas['PRODUCTO']=="Diesel B5 S-50")&
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('GASOLINA 97'))&  
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('GASOHOL 97 PLUS'))&  
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('GASOLINA 98 BA'))& 
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('GASOHOL 98 PLUS'))&  
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('Diesel B5 S-50 GE'))&                                                                                
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('SOLVENTE'))&  
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('GASOLINA 100 LL'))&  
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('GASOLINA 98'))&  
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('CGN SOLVENTE'))&  
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('DIESEL 2'))& 
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('PRODUCTO'))&
+                                        ~(Precios_Mayoristas['PRODUCTO'].str.contains('Diesel 2'))
+                                        ]
+
+
+#codificar producto
+Precios_Mayoristas.loc[Precios_Mayoristas.PRODUCTO=="GASOLINA REGULAR", 'COD_PROD'] = 62
+Precios_Mayoristas.loc[Precios_Mayoristas.PRODUCTO=="GASOLINA PREMIUM", 'COD_PROD'] = 61
+Precios_Mayoristas.loc[Precios_Mayoristas.PRODUCTO=="GASOHOL REGULAR", 'COD_PROD'] = 60
+Precios_Mayoristas.loc[Precios_Mayoristas.PRODUCTO=="GASOHOL PREMIUM", 'COD_PROD'] = 59
+Precios_Mayoristas.loc[Precios_Mayoristas.PRODUCTO=="Cilindros de 10 Kg de GLP", 'COD_PROD'] = 29
+Precios_Mayoristas.loc[Precios_Mayoristas.PRODUCTO=="GLP - G", 'COD_PROD'] = 30
+Precios_Mayoristas.loc[Precios_Mayoristas.PRODUCTO=="DIESEL B5 S-50 UV", 'COD_PROD'] = 15
+Precios_Mayoristas.loc[Precios_Mayoristas.PRODUCTO=="DIESEL B5 UV", 'COD_PROD'] = 9
+Precios_Mayoristas.loc[Precios_Mayoristas.PRODUCTO=="Diesel B5 S-50 UV", 'COD_PROD'] = 15
+Precios_Mayoristas.loc[Precios_Mayoristas.PRODUCTO=="DIESEL B5 UV", 'COD_PROD'] = 9
+
+# Declarar fecha
+
+Precios_Mayoristas['FECHA DE REGISTRO'] = pd.to_datetime(Precios_Mayoristas['FECHA DE REGISTRO'], format='%Y-%m-%d')
+
+Precios_Mayoristas['DIA'] =Precios_Mayoristas['FECHA DE REGISTRO'].dt.day.astype(str)
+Precios_Mayoristas['MES'] =Precios_Mayoristas['FECHA DE REGISTRO'].dt.month.astype(str)
+Precios_Mayoristas['AÑO'] =Precios_Mayoristas['FECHA DE REGISTRO'].dt.year.astype(str)
+
+
+#mes
+
+Precios_Mayoristas['MES']='0'+Precios_Mayoristas['MES']
+Precios_Mayoristas['MES']=Precios_Mayoristas['MES'].str.replace('010','10')
+Precios_Mayoristas['MES']=Precios_Mayoristas['MES'].str.replace('011','11')
+Precios_Mayoristas['MES']=Precios_Mayoristas['MES'].str.replace('012','12')
+
+#día
+
+
+Precios_Mayoristas['fecha_stata']=Precios_Mayoristas['AÑO']+"-"+Precios_Mayoristas['MES']+"-"+Precios_Mayoristas['DIA']
+Precios_Mayoristas['fecha_stata'] = pd.to_datetime(Precios_Mayoristas['fecha_stata'], format='%Y-%m-%d')
+
+
+# Llenar valores nan de GLP-E principalmente
+
+Precios_Mayoristas['PRECIO DE VENTA (SOLES)'] = Precios_Mayoristas['PRECIO DE VENTA (SOLES)'].fillna((Precios_Mayoristas['PRECIO_MIN (SOLES)'] + Precios_Mayoristas['PRECIO_MAX (SOLES)'])/2)
+
+def replace_value(row):
+    if row['PRECIO DE VENTA (SOLES)'] > 100 and not np.isnan(row['PRECIO_MIN (SOLES)']) and not np.isnan(row['PRECIO_MAX (SOLES)']):
+        return (row['PRECIO_MIN (SOLES)'] + row['PRECIO_MAX (SOLES)'])/2
+    else:
+        return row['PRECIO DE VENTA (SOLES)']
+
+# Apply the function to the DataFrame
+Precios_Mayoristas['PRECIO DE VENTA (SOLES)'] = Precios_Mayoristas.apply(replace_value, axis=1)
+
+Precios_Mayoristas = Precios_Mayoristas[Precios_Mayoristas['PRECIO DE VENTA (SOLES)'] <= 100]
+
+
+#Conversión de litros a galones
+Precios_Mayoristas.reset_index()
+
+
+def replace_value_litros(row):
+    if row['PRECIO DE VENTA (SOLES)'] > 5 and row['COD_PROD'] == 30:
+        return (row['PRECIO DE VENTA (SOLES)'])/ 3.78533
+    else:
+        return row['PRECIO DE VENTA (SOLES)']
+
+
+# Apply the function to the DataFrame
+Precios_Mayoristas['PRECIO DE VENTA (SOLES)'] = Precios_Mayoristas.apply(replace_value_litros, axis=1)
+
+
+Precios_Mayoristas.loc[Precios_Mayoristas['COD_PROD'] == 30, 'PRECIO DE VENTA (SOLES)'] = Precios_Mayoristas.loc[Precios_Mayoristas['COD_PROD'] == 30, 'PRECIO DE VENTA (SOLES)'] /  0.5324
+
+# Redondear precios a dos decimales
+
+Precios_Mayoristas['PRECIO DE VENTA (SOLES)']=Precios_Mayoristas['PRECIO DE VENTA (SOLES)'].round(2)
+
+# Guardar data de precios mayoristas sin imputar
+
+Precios_Mayoristas.to_csv("../data/interim/mayoristas_pre_imp.csv", sep=";", encoding="utf-8")
+
+
+
+
+
