931e816244ac9f5593eca64e2a47877b408f8564
Hans Aguirre <81196895+hansaguirre1@users.noreply.github.com>
2024-01-17 12:17:54 -0500

    Hans 2 2024.01.17

--- scripts/ma2_ imputación de precios.py
+++ scripts/ma2_ imputación de precios.py
@@ -20,8 +20,9 @@ df['fecha_stata'] = pd.to_datetime(df['fecha_stata'], infer_datetime_format=True
 df["RUC-prov"] = df["RUC"].astype(str)+"-"+df["PROVINCIA"]
 df.COD_PROD.value_counts()
 cod_prods = [ 46, 45, 37, 36, 47, 48, 28, 19]
+
 #Completar precios y dias por producto
-""" for k in cod_prods:
+for k in cod_prods:
     print(k)
     df_2 = df.copy()
     df_2 = df_2[df_2['COD_PROD'] == k]
@@ -57,55 +58,7 @@ cod_prods = [ 46, 45, 37, 36, 47, 48, 28, 19]
     #df_2.rename(columns={'CODIGOOSINERG': 'COD'}, inplace=True)
     df_2.drop(columns=['PRECIOVENTAx', 'PRECIOVENTA_','dias_faltantes'], inplace=True)
     df_2["COD_PROD"] = k
-    df_2.to_csv(f"..\\data\\interim\\precios mayoristas\\base_final_{k}.csv",  encoding="utf-8",  index=False) """
-
-
-for k in cod_prods:
-    print(k)
-    try:
-        df_2 = df.copy()
-        df_2 = df_2[df_2['COD_PROD'] == k]
-        df_2['PRECIOVENTA_'] = df_2['PRECIOVENTA']
-        df_2 = df_2.groupby(['fecha_stata', "RUC-prov"]).agg({'PRECIOVENTA': 'mean', 'PRECIOVENTA_': 'last'}).reset_index()
-        df_2 = df_2.sort_values(['RUC-prov', 'fecha_stata'])
-        df_2['dias_faltantes'] = (df_2['fecha_stata'].diff()).dt.days - 1
-        df_2.loc[df_2["dias_faltantes"] < 0, "dias_faltantes"] = np.nan
-        df_2['dias_faltantes'] = df_2['dias_faltantes'].fillna(0)
-        fecha_minima = df_2['fecha_stata'].min()
-        fecha_maxima = df_2['fecha_stata'].max()
-        rango_fechas_completo = pd.date_range(fecha_minima, fecha_maxima, freq='D')
-        combinaciones = pd.DataFrame([(id, fecha) for id in df_2['RUC-prov'].unique(
-        ) for fecha in rango_fechas_completo], columns=['RUC-prov', 'fecha_stata'])
-        df_2 = pd.merge(combinaciones, df_2, on=['RUC-prov', 'fecha_stata'], how='outer')
-        df_2 = df_2.sort_values(by=['RUC-prov', 'fecha_stata'])
-        df_2 = df_2.reset_index(drop=True)
-        num_cpus = os.cpu_count()
-        #with ProcessPoolExecutor(max_workers=num_cpus) as executor:
-        #    executor.map(process2, ["PRECIOVENTA_","dias_faltantes"])
-        process2("PRECIOVENTA_")
-        process2("dias_faltantes")
-        df_2['PRECIOVENTAx'] = df_2['PRECIOVENTA_']
-        # q = 1
-        # for i in range(1, len(df_2)):
-        #     if q < 100:
-        #         if (df_2.at[i, 'RUC-prov'] == df_2.at[i-1, 'RUC-prov']) and pd.isnull(df_2.at[i, 'PRECIOVENTA']):
-        #             df_2.at[i, 'PRECIOVENTA'] = df_2.at[i-1, 'PRECIOVENTA']
-        #         q += 1
-        
-        #     if i < len(df_2) - 1 and not pd.isnull(df_2.at[i+1, 'PRECIOVENTA']):
-        #         q = 1
-        mask = (df_2['RUC-prov'] == df_2['RUC-prov'].shift(1)) & pd.isnull(df_2['PRECIOVENTA'])    
-        df_2['PRECIOVENTA'] = df_2['PRECIOVENTA'].mask(mask, df_2['PRECIOVENTA'].ffill())    
-        reset_mask = ~pd.isnull(df_2['PRECIOVENTA'].shift(-1))    
-        df_2['q'] = (reset_mask.cumsum() % 14) + 1    
-        df_2 = df_2[df_2['q'] < 14].copy()
-        df_2 = df_2.drop(columns=['q'])
-
-        df_2.drop(columns=['PRECIOVENTAx', 'PRECIOVENTA_','dias_faltantes'], inplace=True)
-        df_2["COD_PROD"] = k
-        df_2.to_csv(f"../data/interim/precios mayoristas/base_final_{k}.csv", index=False, encoding="utf-8")
-    except:
-        pass
+    df_2.to_csv(f"..\\data\\interim\\precios mayoristas\\base_final_{k}.csv",  encoding="utf-8",  index=False)
 
 
 # guardar todos los productos en una lista
