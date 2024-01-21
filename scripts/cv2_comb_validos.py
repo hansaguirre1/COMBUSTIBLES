f88521e98de798b8a97288f7e75fd9186f49276c
andyterr170796 <70968080+andyterr170796@users.noreply.github.com>
2024-01-21 03:00:49 -0500

    a

--- scripts/cv2_comb_validos.py
+++ scripts/cv2_comb_validos.py
@@ -27,11 +27,11 @@ deps = df["DEPARTAMENTO"].unique()
 aos = df["AÑO"].unique()
 combinaciones = list(product(deps, aos))
 df_gnv = pd.DataFrame(combinaciones, columns=['DEPARTAMENTO', 'AÑO'])
-df_gnv["COD_PROD"] = 16
+df_gnv["COD_PROD"] = nom_prods[b]
 df_glpg = pd.DataFrame(combinaciones, columns=['DEPARTAMENTO', 'AÑO'])
-df_glpg["COD_PROD"] = 30
+df_glpg["COD_PROD"] = nom_prods[h]
 df_glpe = pd.DataFrame(combinaciones, columns=['DEPARTAMENTO', 'AÑO'])
-df_glpe["COD_PROD"] = 29
+df_glpe["COD_PROD"] = nom_prods[g]
 
 #df['VOLUMENES'] = pd.to_numeric(df['VOLUMENES'].str.replace(',', ''), errors='coerce')
 
@@ -40,17 +40,17 @@ df.loc[df['VOLUMENES'] == 0, 'VOLUMENES'] = pd.NA
 
 
 df_gasohol_premium = df[(df['COMBUSTIBLE'] == 'GASOHOL PREMIUM') | (df['COMBUSTIBLE'] == 'Gasohol 95 Plus')]
-df_gasohol_premium = combse(df_gasohol_premium,59)
+df_gasohol_premium = combse(df_gasohol_premium,nom_prods[c])
 df_gasolina_regular = df[(df['COMBUSTIBLE'] == 'GASOLINA REGULAR') | (df['COMBUSTIBLE'] == 'Gasolina 90')]
-df_gasolina_regular = combse(df_gasolina_regular,62)
+df_gasolina_regular = combse(df_gasolina_regular,nom_prods[f])
 df_gasolina_premium = df[(df['COMBUSTIBLE'] == 'GASOLINA PREMIUM') | (df['COMBUSTIBLE'] == 'Gasolina 95')]
-df_gasolina_premium = combse(df_gasolina_premium,61)
+df_gasolina_premium = combse(df_gasolina_premium,nom_prods[e])
 df_gasohol_regular = df[(df['COMBUSTIBLE'] == 'GASOHOL REGULAR') | (df['COMBUSTIBLE'] == 'Gasohol 90 Plus')]
-df_gasohol_regular = combse(df_gasohol_regular,60)
+df_gasohol_regular = combse(df_gasohol_regular,nom_prods[d])
 df_diesel = df[(df['COMBUSTIBLE'] == 'DIESEL B5 UV') | (df['COMBUSTIBLE'] == 'DB5 S-50')]
-df_diesel = combse(df_diesel,15)
+df_diesel = combse(df_diesel,nom_prods[a])
 df_diesel2 = df[(df['COMBUSTIBLE'] == 'DIESEL B5 S-50 UV') | (df['COMBUSTIBLE'] == 'Diesel B5')]
-df_diesel2 = combse(df_diesel2,9)
+df_diesel2 = combse(df_diesel2,nom_prods[m])
 
 combs = pd.concat([df_diesel,df_diesel2,df_gnv,df_glpe,df_glpg,df_gasohol_premium, df_gasolina_regular,df_gasolina_premium, df_gasohol_regular], ignore_index=True)
 combs["ok"] = 1
